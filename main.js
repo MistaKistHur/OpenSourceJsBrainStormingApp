2d2d93bde67dfd4f896fe451394eaf0ce161aea3
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-11-26 23:04:33 +0000

    Template for later.

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
+// ::>> Notes:
+// ::>> Notes:
+// ::>> Notes:
+"use-strict";
+document.addEventListener("DOMContentLoaded",()=>{
+  mouseTrack(x,y)=>{
+    document.addEventListener("mousemove",()=>{
+      clientX;
+      clientY;
+    }){
+    
+    }
+  } // Closing bracket for Mouse Tracker    
+// ::>> Notes:
+// ::>> Notes:  
+// ::>> Notes:  
+// ::>> Notes:  
+  class InitialIdea{
+    constructor(){
+      this.initialIdea = '';
+      this.assoc = 'association';
+      this.bulletPoints = [];      
+      this.abstract = '';      
+      this.details = {};            // ::>> \Object..      
+      this.subject = '';
+      this.project = '';
+      this.assets = [];      
+      this.tags = [];
+      this.team = '';
+      this.id = '';
+      this.value = null;
+      this.pub = bool;
+    }
+    function AddAssets(){
+          // ::>> Add jpgs, pngs, svgs, ani, txt, pdf etc....... 
+    }
+    function PopAssoc(){
+          // ::>> Push items to the Assosiations list.
+    }
+    function BuildTreeRoots(){
+          // ::>> Create initial Tree Sapling.[idea Bubble.] 
+    }
+    function Branch(){
+          // ::>> Spawn A  
+    }
+   }      // ::>> Closing Bracket for: class InitialIdea  
+   class BubbleBranch{
+    constructor(){
+    
+    }
+   }      // ::>> Closing Bracket for: class BubbleBranch.
+  
+  element.animate([?]);
+  
+  //::>> Not sure if its possible to invoke a python script via js:> search spider..... 
+  
+
+
+
+
+}); // Closing Bracket for DOM evt.
+
+
+
+
 
