026bdb1123eb92d05507766acf6444f63f1898de
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-11-26 23:47:17 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 // ::>> Notes:
 // ::>> Notes:
-// ::>> Notes:
+// ::>> Notes:  Which to use for Animation.> Svg? Canvas? WebGl? GameEngine? P5Js?....
 "use-strict";
 document.addEventListener("DOMContentLoaded",()=>{
   mouseTrack(x,y)=>{
     document.addEventListener("mousemove",()=>{
-      clientX;
-      clientY;
-    }){
-    
-    }
+      let X = clientX;
+      let Y = clientY;
+      console.log('Coords: X::>> Y::>> ');
+    });
   } // Closing bracket for Mouse Tracker    
 // ::>> Notes:
 // ::>> Notes:  
 // ::>> Notes:  
 // ::>> Notes:  
+
   class InitialIdea{
     constructor(){
       this.initialIdea = '';
-      this.assoc = 'association';
-      this.bulletPoints = [];      
-      this.abstract = '';      
+      this.assoc = 'association';   // ::>> this could either be an object, an array or an ext json file.
+      this.bulletPoints = [];       // ::>> \add     
+      this.abstract = '';           // ::>> \general      
       this.details = {};            // ::>> \Object..      
-      this.subject = '';
-      this.project = '';
-      this.assets = [];      
-      this.tags = [];
-      this.team = '';
-      this.id = '';
-      this.value = null;
-      this.pub = bool;
+      this.subject = '';            // ::>> \pick
+      this.project = '';            // ::>> \is this idea associated with any specific project.
+      this.assets = [];             // ::>> \?      
+      this.tags = [];               // ::>> \Stack Overflow style tagging System.
+      this.team = '';               // ::>> \If Applicable
+      this.id = '';                 // ::>> \Guid
+      this.value = null;            // ::>> \Use this for something.
+      this.pub = bool;              // ::>> \Public or private.
     }
     function AddAssets(){
           // ::>> Add jpgs, pngs, svgs, ani, txt, pdf etc....... 
@@ -37,30 +37,40 @@ document.addEventListener("DOMContentLoaded",()=>{
     function PopAssoc(){
           // ::>> Push items to the Assosiations list.
     }
-    function BuildTreeRoots(){
+    function BuildTreeRoots(){                // Svg Tree with canvas bubbles, bubbling up around it.
           // ::>> Create initial Tree Sapling.[idea Bubble.] 
     }
     function Branch(){
-          // ::>> Spawn A  
+          // ::>> Spawn A branch. 
     }
    }      // ::>> Closing Bracket for: class InitialIdea  
    class BubbleBranch{
     constructor(){
-    
+      this.active = null;
+      this.budding = null;
+      this.nodes = null;
+      this.attatch = function(date,time,usr,pin,promt);
     }
    }      // ::>> Closing Bracket for: class BubbleBranch.
   
   element.animate([?]);
+                   
+                   // Graphs.js
+                   //
   
   //::>> Not sure if its possible to invoke a python script via js:> search spider..... 
-  
-
-
-
+  //::>> Populate Relevant ideas, based on first inputs.
+  //::>> Show user other bubbles base on their ideas.
+  //::>> use ml5 nueral network empty template.
 
 }); // Closing Bracket for DOM evt.
-
-
-
-
+                   
+//. ::>> More Notes:
+//. ::>> More Notes: 
+//. ::>> More Notes: 
+//. ::>> More Notes: 
+//. ::>> More Notes:
+//. ::>> More Notes: 
+//. ::>> More Notes: 
+//. ::>> More Notes:
 
